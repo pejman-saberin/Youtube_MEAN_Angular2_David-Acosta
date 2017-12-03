3debf3a497a43c408e67e9c072b1b4f351abe66c
pejman <naderpejman@yahoo.com>
2017-12-03 13:19:23 -0700

    Adding mongoose
    Copy and paste from // copied from http://mongoosejs.com/

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');  //copy and paste from https://expressjs.com/en/4x/api.html
 const app = express();
-
+// copied from http://mongoosejs.com/
+const mongoose = require('mongoose');  //using const is the ES6 syntex
+mongoose.connect('mongodb://localhost/test', { useMongoClient: true });
+mongoose.Promise = global.Promise; //this supress the warning in the consil
 
 app.get('/', (req, res)=>{  ///putting * instead of /  , includes all the paths.
   res.send('<h1>hello world</h1>');
