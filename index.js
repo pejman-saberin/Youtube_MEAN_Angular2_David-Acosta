62d436e0776b29ef32a876c26eb5249e9f4a09de
pejman <naderpejman@yahoo.com>
2017-12-03 14:24:05 -0700

    Installing angular front end
    npm install -g @angular/cli  -->to install angular
    ng new client
    cd client/
    ng serve
    if had issue when ran ng serve., delete both the package-lock.json and the node-modules folder and run npm install

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const app = express();
 // copied from http://mongoosejs.com/
 const mongoose = require('mongoose');  //using const is the ES6 syntex
 const config=require('./config/database');
-mongoose.Promise = global.Promise; //this supress the warning in the consil
+mongoose.Promise = global.Promise; //this supress the warning in the console
 
-mongoose.connect(config.uri, (err)=>{
+mongoose.connect(config.uri,  { useMongoClient: true },(err)=>{  //added  { useMongoClient: true } to supress the message in the console
   if(err){
     console.log('Could Not connect to database: ',err);
   }else{
+    //console.log(config.secret); to test the crypto
     console.log('Connected to database: '+config.db);
   }
 });
