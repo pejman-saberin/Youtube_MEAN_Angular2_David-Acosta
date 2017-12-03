31efb3affb9e3a29e997a22af443ece39871fa2b
pejman <naderpejman@yahoo.com>
2017-12-03 16:51:20 -0700

    Start of video 2: MEAN Stack Tutorial with Angular 2 - Part 2: Angular 2 Routing and Navigation
    Start of the video 2.. Adding router
    https://www.youtube.com/watch?v=jf-6dzQg1To&list=PL3vQyqzqjZ62zMxa0veah_CHMgwOM4JWD&index=2

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { RouterModule, Routes } from '@angular/router';  //pasted directly from https://angular.io/guide/router
+import { NgModule } from '@angular/core';
+
+
+@NgModule({
+  declarations: [ ],
+  imports: [ ],
+  providers: [],
+  bootstrap: []
+})
+
+export class AppRoutingModule { }
