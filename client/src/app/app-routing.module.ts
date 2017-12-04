ce49adfc76b3640157265dc81b1b1446f1740e12
pejman <naderpejman@yahoo.com>
2017-12-03 19:23:41 -0700

    Adding dashbord link and using routerlink for the navbar links
    ng generate component dashbaord

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,15 +1,22 @@
 import { RouterModule, Routes } from '@angular/router';  //pasted directly from https://angular.io/guide/router
 import { NgModule } from '@angular/core';
 import {HomeComponent} from './components/home/home.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
 
 const appRoutes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    pathMatch: 'full'
   },
 
   //{ path: '',   redirectTo: '/heroes', pathMatch: 'full' },
-  { path: '**', component: HomeComponent }  //any other route redirect to homepage
+  { path: '**', component: HomeComponent,pathMatch: 'full' }  //any other route redirect to homepage
 ];
 
 @NgModule({
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,13 +5,15 @@ import {AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent  //this is generated automatically using ng generate component navbar  .
+    HomeComponent,
+    DashboardComponent  //this is generated automatically using ng generate component navbar  .
    ],
   imports: [
     BrowserModule,
