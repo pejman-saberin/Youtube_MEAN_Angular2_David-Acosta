13432184cd64cf9637a6e38929b527eb2b1934a6
pejman <naderpejman@yahoo.com>
2017-12-09 14:27:14 +0000

    adding local storage in front end and login function implementation

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup,Validators, FormControl } from '@angular/forms';
+import {AuthService} from '../../services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +10,7 @@ import { FormBuilder, FormGroup,Validators, FormControl } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private formBuilder:FormBuilder) {this.createForm ();}
+  constructor(private formBuilder:FormBuilder, private authservice: AuthService, private router: Router ) {this.createForm ();}
 
   ngOnInit() {
   }
@@ -36,12 +38,28 @@ export class LoginComponent implements OnInit {
   }
   
   onLoginSubmit(){
-    this.processing=true;
-    this.disableForm();
-    const user={
-      username: this.form.get('username').value,
-      password: this.form.get('password').value,
-    }
+      this.processing=true;
+      this.disableForm();
+      const user={
+        username: this.form.get('username').value,
+        password: this.form.get('password').value,
+      }
+    
+      this.authService.login(user).subscribe(data=>{
+      if(!data.success){
+        this.messageClass='alert alert-danger';
+        this.message=data.message;
+        this.processing=false;
+        this.enableForm();
+      }else{
+        this.messageClass='alert alert-success';
+        this.message=data.message;
+        this.authService.storeUserData(data.token,data.user);
+        setTimeout((=>{
+          this.router.navigate(['/dashboard']);
+        },2000);
+      }
+    })
   }
   
   
