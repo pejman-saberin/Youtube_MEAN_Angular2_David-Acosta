1b582ed3295f3fb7074e8a173cf11c0ea6e8d2df
pejman <naderpejman@yahoo.com>
2017-12-09 08:20:02 -0700

    fixed onLoginSubmit() in login component and added login route to navbar

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -10,58 +10,63 @@ import {Router} from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private formBuilder:FormBuilder, private authservice: AuthService, private router: Router ) {this.createForm ();}
+  constructor(private formBuilder:FormBuilder, private authService: AuthService, private router: Router ) {this.createForm ();}
 
   ngOnInit() {
   }
-  
+
   messageClass;
   message;
   processing=false;
   form:FormGroup;
-  
+
   createForm(){
     this.form=this.formBuilder.group({
       username: ['', Validators.required],
       password: ['', Validators.required],
     });
   }
-  
+
   disableForm(){
     this.form.controls['username'].disable();
     this.form.controls['password'].disable();
   }
-  
+
   enableForm(){
     this.form.controls['username'].enable();
     this.form.controls['password'].enable();
   }
-  
-  onLoginSubmit(){
-      this.processing=true;
-      this.disableForm();
-      const user={
-        username: this.form.get('username').value,
-        password: this.form.get('password').value,
+
+  // Function to submit form and login user
+  onLoginSubmit() {
+    this.processing = true; // Used to submit button while is being processed
+    this.disableForm(); // Disable form while being process
+    // Create user object from user's input
+    const user = {
+      username: this.form.get('username').value, // Username input field
+      password: this.form.get('password').value // Password input field
+    }
+
+    // Function to send login data to API
+    this.authService.login(user).subscribe(data => {
+      // Check if response was a success or error
+      if (!data.success) {
+        this.messageClass = 'alert alert-danger'; // Set bootstrap error class
+        this.message = data.message; // Set error message
+        this.processing = false; // Enable submit button
+        this.enableForm(); // Enable form for editting
+      } else {
+        this.messageClass = 'alert alert-success'; // Set bootstrap success class
+        this.message = data.message; // Set success message
+        // Function to store user's token in client local storage
+        this.authService.storeUserData(data.token, data.user);
+        // After 2 seconds, redirect to dashboard page
+        setTimeout(() => {
+          this.router.navigate(['/dashboard']); // Navigate to dashboard view
+        }, 2000);
       }
-    
-      this.authService.login(user).subscribe(data=>{
-      if(!data.success){
-        this.messageClass='alert alert-danger';
-        this.message=data.message;
-        this.processing=false;
-        this.enableForm();
-      }else{
-        this.messageClass='alert alert-success';
-        this.message=data.message;
-        this.authService.storeUserData(data.token,data.user);
-        setTimeout((=>{
-          this.router.navigate(['/dashboard']);
-        },2000);
-      }
-    })
+    });
   }
-  
-  
-  
+
+
 }
