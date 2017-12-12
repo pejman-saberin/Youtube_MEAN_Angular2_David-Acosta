e27e1cd7f30a0b677a2379ee3aae02f3450c175d
pejman <naderpejman@yahoo.com>
2017-12-12 08:34:45 -0700

    min 33 front end create form when you click post comments
    Boolean causes it for rest of the fields to get hidden and only show the title an body box for the new form.
    Then window.location.reload(); //reloads the page once submitted or go back is pressed. On the next, submit will do a api call

--- client/src/app/components/blog/blog.component.ts
+++ client/src/app/components/blog/blog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl,FormGroup,FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-blog',
@@ -11,8 +12,50 @@ export class BlogComponent implements OnInit {
   messageClass;
   newPost=false;
   loadingBlogs=false;
+  form;
+  processing=false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    this.createNewBlogForm(); //when component loads it creates the form for us
+  }
+
+  createNewBlogForm(){
+    this.form=this.formBuilder.group({
+      title:['',Validators.compose([
+        Validators.required,
+        Validators.maxLength(50),
+        Validators.minLength(5),
+        this.alphaNumricValidation
+      ])],
+      body:['',Validators.compose([
+        Validators.required,
+        Validators.maxLength(500),
+        Validators.minLength(5),
+
+      ])]
+    })
+  }
+
+  enableFormNewBlogForm(){
+    this.form.get('title').enable();
+    this.form.get('body').enable();
+  }
+
+  // Disable new blog form
+   disableFormNewBlogForm() {
+     this.form.get('title').disable(); // Disable title field
+     this.form.get('body').disable(); // Disable body field
+   }
+
+  //cusome validation
+  alphaNumricValidation(controls){
+    const regExp = new RegExp(/^[a-zA-Z0-9 ]+$/);// this is taken straight from the backend
+    if(regExp.test(controls.value)){
+      return null;
+    } else{
+      return {'alphaNumricValidation':true}
+    }
+  }
 
   ngOnInit() {
   }
@@ -25,6 +68,18 @@ export class BlogComponent implements OnInit {
     setTimeout(()=>{
       this.loadingBlogs=false;
     },4000);
+  }
+
+  draftComment(){
+
+  }
+
+  onBlogSubmit(){
+    console.log('form submitted');
+  }
+
+  goBack(){
+    window.location.reload(); //reloads the page
   }
 
 
