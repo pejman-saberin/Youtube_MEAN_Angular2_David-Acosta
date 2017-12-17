f06be732f7c59129a4fd93c7a0eb5a2bec8e8173
pejman <naderpejman@yahoo.com>
2017-12-17 08:33:18 -0700

    Some parts are copy and post

--- client/src/app/components/blog/blog.component.ts
+++ client/src/app/components/blog/blog.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl,FormGroup,FormBuilder, Validators} from '@angular/forms';
-import {AuthService} from '../../services/auth.service';
-import {BlogService} from '../../services/blog.service'
-
-
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { BlogService } from '../../services/blog.service';
 
 @Component({
   selector: 'app-blog',
@@ -12,122 +10,166 @@ import {BlogService} from '../../services/blog.service'
 })
 export class BlogComponent implements OnInit {
 
-  message;
   messageClass;
-  newPost=false;
-  loadingBlogs=false;
+  message;
+  newPost = false;
+  loadingBlogs = false;
   form;
-  processing=false;
+  commentForm;
+  processing = false;
   username;
   blogPosts;
-  newComment;
+  newComment = [];
+  enabledComments = [];
 
-  constructor(private formBuilder: FormBuilder,
-              private authService:AuthService,
-              private blogService:BlogService) {
-    this.createNewBlogForm(); //when component loads it creates the form for us
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private blogService: BlogService
+  ) {
+    this.createNewBlogForm(); // Create new blog form on start up
+    this.createCommentForm(); // Create form for posting comments on a user's blog post
   }
 
-  ngOnInit() {
-    this.authService.getProfile().subscribe(profile=>{
-      this.username=profile.user.username;
-    });
-    this.getAllBlogs();
-  }
-
-
-  createNewBlogForm(){
-    this.form=this.formBuilder.group({
-      title:['',Validators.compose([
+  // Function to create new blog form
+  createNewBlogForm() {
+    this.form = this.formBuilder.group({
+      // Title field
+      title: ['', Validators.compose([
         Validators.required,
         Validators.maxLength(50),
         Validators.minLength(5),
-        this.alphaNumricValidation
+        this.alphaNumericValidation
       ])],
-      body:['',Validators.compose([
+      // Body field
+      body: ['', Validators.compose([
         Validators.required,
         Validators.maxLength(500),
-        Validators.minLength(5),
-
+        Validators.minLength(5)
       ])]
     })
   }
 
-  enableFormNewBlogForm(){
-    this.form.get('title').enable();
-    this.form.get('body').enable();
+  // Create form for posting comments
+  createCommentForm() {
+    this.commentForm = this.formBuilder.group({
+      comment: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(200)
+      ])]
+    })
+  }
+
+  // Enable the comment form
+  enableCommentForm() {
+    this.commentForm.get('comment').enable(); // Enable comment field
+  }
+
+  // Disable the comment form
+  disableCommentForm() {
+    this.commentForm.get('comment').disable(); // Disable comment field
+  }
+
+  // Enable new blog form
+  enableFormNewBlogForm() {
+    this.form.get('title').enable(); // Enable title field
+    this.form.get('body').enable(); // Enable body field
   }
 
   // Disable new blog form
-   disableFormNewBlogForm() {
-     this.form.get('title').disable(); // Disable title field
-     this.form.get('body').disable(); // Disable body field
-   }
+  disableFormNewBlogForm() {
+    this.form.get('title').disable(); // Disable title field
+    this.form.get('body').disable(); // Disable body field
+  }
 
-  //cusome validation
-  alphaNumricValidation(controls){
-    const regExp = new RegExp(/^[a-zA-Z0-9 ]+$/);// this is taken straight from the backend
-    if(regExp.test(controls.value)){
-      return null;
-    } else{
-      return {'alphaNumricValidation':true}
+  // Validation for title
+  alphaNumericValidation(controls) {
+    const regExp = new RegExp(/^[a-zA-Z0-9 ]+$/); // Regular expression to perform test
+    // Check if test returns false or true
+    if (regExp.test(controls.value)) {
+      return null; // Return valid
+    } else {
+      return { 'alphaNumericValidation': true } // Return error in validation
     }
   }
 
-
-  newBlogForm(){
-    this.newPost=true;
-  }
-  reloadBlogs(){
-    this.loadingBlogs=true; //hide teh reload button for 4 seconds
-    this.getAllBlogs();
-    setTimeout(()=>{
-      this.loadingBlogs=false;
-    },4000);
+  // Function to display new blog form
+  newBlogForm() {
+    this.newPost = true; // Show new blog form
   }
 
-  draftComment(){
-
+  // Reload blogs on current page
+  reloadBlogs() {
+    this.loadingBlogs = true; // Used to lock button
+    this.getAllBlogs(); // Add any new blogs to the page
+    setTimeout(() => {
+      this.loadingBlogs = false; // Release button lock after four seconds
+    }, 4000);
   }
 
-  onBlogSubmit(){
-    //console.log('form submitted');
-    this.processing=true; //disables on submission
-    this.disableFormNewBlogForm();
+  // Function to post a new comment on blog post
+  draftComment(id) {
+    this.commentForm.reset(); // Reset the comment form each time users starts a new comment
+    this.newComment = []; // Clear array so only one post can be commented on at a time
+    this.newComment.push(id); // Add the post that is being commented on to the array
+  }
 
-    const blog={
-      title: this.form.get('title').value,
-      body: this.form.get('body').value,
-      createdBy:this.username
+  // Function to cancel new post transaction
+  cancelSubmission(id) {
+    const index = this.newComment.indexOf(id); // Check the index of the blog post in the array
+    this.newComment.splice(index, 1); // Remove the id from the array to cancel post submission
+    this.commentForm.reset(); // Reset  the form after cancellation
+    this.enableCommentForm(); // Enable the form after cancellation
+    this.processing = false; // Enable any buttons that were locked
+  }
+
+  // Function to submit a new blog post
+  onBlogSubmit() {
+    this.processing = true; // Disable submit button
+    this.disableFormNewBlogForm(); // Lock form
+
+    // Create blog object from form fields
+    const blog = {
+      title: this.form.get('title').value, // Title field
+      body: this.form.get('body').value, // Body field
+      createdBy: this.username // CreatedBy field
     }
-    this.blogService.newBlog(blog).subscribe(data=>{
-      if(!data.success){
-        this.messageClass='alert alert-danger';
-        this.message=data.message;
-        this.processing=false;
-        this.enableFormNewBlogForm();
-      } else{
-        this.messageClass='alert alert-success';
-        this.message=data.message;
-        this.getAllBlogs();  //everytime a new blog is posted get all the blogs inclusing the new one
-        setTimeout(()=>{
-          this.newPost=false;
-          this.processing=false;
-          this.message=false;
-          this.form.reset();
-          this.enableFormNewBlogForm();
-        },2000)
+
+    // Function to save blog into database
+    this.blogService.newBlog(blog).subscribe(data => {
+      // Check if blog was saved to database or not
+      if (!data.success) {
+        this.messageClass = 'alert alert-danger'; // Return error class
+        this.message = data.message; // Return error message
+        this.processing = false; // Enable submit button
+        this.enableFormNewBlogForm(); // Enable form
+      } else {
+        this.messageClass = 'alert alert-success'; // Return success class
+        this.message = data.message; // Return success message
+        this.getAllBlogs();
+        // Clear form data after two seconds
+        setTimeout(() => {
+          this.newPost = false; // Hide form
+          this.processing = false; // Enable submit button
+          this.message = false; // Erase error/success message
+          this.form.reset(); // Reset all form fields
+          this.enableFormNewBlogForm(); // Enable the form fields
+        }, 2000);
       }
     });
   }
 
-  goBack(){
-    window.location.reload(); //reloads the page
+  // Function to go back to previous page
+  goBack() {
+    window.location.reload(); // Clear all variable states
   }
 
-  getAllBlogs(){
-    this.blogService.getAllBlogs().subscribe(data=>{
-      this.blogPosts=data.blogs;
+  // Function to get all blogs from the database
+  getAllBlogs() {
+    // Function to GET all blogs from database
+    this.blogService.getAllBlogs().subscribe(data => {
+      this.blogPosts = data.blogs; // Assign array to use in HTML
     });
   }
 
@@ -147,10 +189,41 @@ export class BlogComponent implements OnInit {
     });
   }
 
-  postcomment(id){
-
+  // Function to post a new comment
+  postComment(id) {
+    this.disableCommentForm(); // Disable form while saving comment to database
+    this.processing = true; // Lock buttons while saving comment to database
+    const comment = this.commentForm.get('comment').value; // Get the comment value to pass to service function
+    // Function to save the comment to the database
+    this.blogService.postComment(id, comment).subscribe(data => {
+      this.getAllBlogs(); // Refresh all blogs to reflect the new comment
+      const index = this.newComment.indexOf(id); // Get the index of the blog id to remove from array
+      this.newComment.splice(index, 1); // Remove id from the array
+      this.enableCommentForm(); // Re-enable the form
+      this.commentForm.reset(); // Reset the comment form
+      this.processing = false; // Unlock buttons on comment form
+      if (this.enabledComments.indexOf(id) < 0) this.expand(id); // Expand comments for user on comment submission
+    });
   }
 
+  // Expand the list of comments
+  expand(id) {
+    this.enabledComments.push(id); // Add the current blog post id to array
+  }
 
+  // Collapse the list of comments
+  collapse(id) {
+    const index = this.enabledComments.indexOf(id); // Get position of id in array
+    this.enabledComments.splice(index, 1); // Remove id from array
+  }
+
+  ngOnInit() {
+    // Get profile username on page load
+    this.authService.getProfile().subscribe(profile => {
+      this.username = profile.user.username; // Used when creating new blog posts and comments
+    });
+
+    this.getAllBlogs(); // Get all blogs on component load
+  }
 
 }
